0bc903acd154844a0473e5590c14bc4c2d046ce0
sytzez <sytzez@gmail.com>
2022-11-18 23:52:36 +0000

    Optimize and simplify hosting index.html

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use actix_cors::Cors;
 use actix_web::http::header::ContentType;
 use actix_web::web::{Data, Form};
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 use std::fs;
 
 struct AppState {
     std_code: String,
+    index_html: String,
 }
 
 #[derive(Deserialize)]
 struct RunRequest {
     code: String,
+}
+
+#[get("/")]
+async fn index_service(state: Data<AppState>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(state.index_html.clone())
 }
 
 #[post("/run")]
@@ -29,8 +37,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(get_cors())
             .app_data(get_app_data())
+            .service(index_service)
             .service(run_service)
-            .service(actix_files::Files::new("/", "./public").index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -40,11 +48,16 @@ async fn main() -> std::io::Result<()> {
 fn get_app_data() -> Data<AppState> {
     Data::new(AppState {
         std_code: get_std_code(),
+        index_html: get_index_html(),
     })
 }
 
 fn get_std_code() -> String {
     fs::read_to_string("../compost/lib/std.compost").expect("Unable to locate std.compost")
+}
+
+fn get_index_html() -> String {
+    fs::read_to_string("./public/index.html").expect("Unable to locate index.html")
 }
 
 fn get_cors() -> Cors {
